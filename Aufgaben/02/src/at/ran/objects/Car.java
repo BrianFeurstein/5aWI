ceff2e5fb6bf0a3fc854e2cda879b0cb034cc753
BrianFeurstein <79904813+BrianFeurstein@users.noreply.github.com>
2022-10-17 14:35:40 +0200

    aufgabe 02 abgeschlossen

---
+++ Aufgaben/02/src/at/ran/objects/Car.java
@@ -0,0 +1,15 @@
+package at.ran.objects;
+
+public class Car {
+    public int fuelConsumption;
+    public int fuelAmount;
+    public String brand;
+    public String serialNumber;
+    private String color;
+
+
+    public void drive(){
+        this.fuelAmount = this.fuelAmount - fuelConsumption;
+        System.out.println("I am driving");
+    }
+}
