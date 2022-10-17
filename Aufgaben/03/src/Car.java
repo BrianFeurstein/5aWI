b439c860713a5bbb274474d4c85d10288f8c96ec
BrianFeurstein <79904813+BrianFeurstein@users.noreply.github.com>
2022-10-17 15:00:42 +0200

    aufgabe 03 abgeschlossen

--- Aufgaben/03/src/Car.java
+++ Aufgaben/03/src/Car.java
@@ -1,5 +1,41 @@
 public class Car {
-    public static void main(String[] args) {
+    public int fuelConsumption;
+    public int fuelAmount;
+    public String brand;
+    public String serialNumber;
+    private String color;
+
+
+    public void drive(){
+        this.fuelAmount = this.fuelAmount - fuelConsumption;
+        System.out.println("I am driving");
+    }
+
+    public void vBreak(){
+        System.out.println("Ich bremse");
+    }
+
+    public void turboBoost(){
+        if(this.fuelAmount > this.fuelAmount*0.1){
+            System.out.println("SuperBoostMode");
+        }
+        else{
+            System.out.println("Not enough fuel to go to Superboost");
+        }
+    }
+
+    public void honk(int amountOfRepetitions){
+        for(int i = 0; i < amountOfRepetitions;i++){
+            System.out.println("TUUUUT");
+        }
+
+
+    }
+
+    public void getRemainingRange(){
+        int range = this.fuelAmount/this.fuelConsumption;
+        System.out.println("range: ");
+        System.out.print(range);
 
     }
 }
---
+++ Aufgaben/03/src/main.java
@@ -0,0 +1,32 @@
+
+public class main {
+    public static void main(String[] args) {
+
+
+        int amountOfRepetitions = 7;
+
+        Car c1 = new Car();
+        c1.brand = "Audi";
+        c1.fuelConsumption = 7;
+        c1.serialNumber = "1234";
+        c1.fuelAmount = 70;
+
+        Car c2 = new Car();
+        c2.brand = "Mercedes";
+        c2.serialNumber = "789";
+        c2.fuelConsumption = 5;
+        c2.fuelAmount = 20;
+
+        /*System.out.println(c2.fuelAmount);
+        c2.drive();
+        System.out.println(c2.fuelAmount);
+
+
+        System.out.println(c1.serialNumber);
+        System.out.println(c2.serialNumber);*/
+        c2.vBreak();
+        c2.turboBoost();
+        c2.honk(amountOfRepetitions);
+        c2.getRemainingRange();
+    }
+}
