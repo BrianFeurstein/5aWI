6e7b1d4af598c2112a99b40cababa8cb45ba16f9
BrianFeurstein <79904813+BrianFeurstein@users.noreply.github.com>
2022-11-15 15:02:01 +0100

    alle aufgaben bis zur Nummer 10 wurden abgeschlossen

--- Aufgaben/10/src/Remote.java
+++ Aufgaben/10/src/Remote.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -65,5 +66,3 @@ public class Remote {
     }
 
 }
-
-
--- Aufgaben/10/src/main.java
+++ Aufgaben/10/src/main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args) {
         Battery b1 = new Battery(28);
         Battery b2 = new Battery(23);
-        Remote r1 = new Remote(true,false);
+        Remote r1 = new Remote(true,true);
         r1.addBattery(b1);
         r1.addBattery(b2);
         r1.getStatus();
