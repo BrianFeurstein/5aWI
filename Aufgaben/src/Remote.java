7e5644d5e8e4a631854954dfdd790597563da4d5
BrianFeurstein <79904813+BrianFeurstein@users.noreply.github.com>
2022-11-15 14:55:14 +0100

    alle Aufgaben bis 10 abgeschlossen

--- Aufgaben/src/Remote.java
+++ Aufgaben/src/Remote.java
@@ -3,45 +3,65 @@ import java.util.List;
 
 public class Remote {
     private boolean isOn;
-    private int hasPower;
+    private boolean hasPower;
     private List<Battery> battery;
 
 
-    public Remote(boolean isOn, int hasPower) {
+    public Remote(boolean isOn, boolean hasPower) {
         this.isOn = isOn;
         this.hasPower = hasPower;
         this.battery = new ArrayList<>();
-    }
-
-    public boolean isOn() {
-        return isOn;
-    }
-
-    public void setOn(boolean on) {
-        isOn = on;
-    }
-
-    public int getHasPower() {
-        return hasPower;
-    }
-
-    public void setHasPower(int hasPower) {
-        this.hasPower = hasPower;
-    }
-
-    public List<Battery> getBattery() {
-        return battery;
     }
 
     public void addBattery(Battery battery){
         this.battery.add(battery);
     }
 
+    public boolean isOn() { return isOn;    }
+
+    public void setOn(boolean on) {
+        isOn = on;
+    }
+
+
+    public boolean isHasPower() {
+        return hasPower;
+    }
+
+    public void setHasPower(boolean hasPower) {
+        this.hasPower = hasPower;
+    }
+
+
+
+    public List<Battery> getBattery() {
+        return battery;
+    }
+
+
     public void getStatus(){
-        System.out.println("Akkustand: " + );
-        System.out.println("Mittelwert: " + );
+        int b1 = battery.get(0).getChargingStatus();
+        int b2 = battery.get(1).getChargingStatus();
+
+        int m = (b1+b2)/2;
+
+        System.out.println("Mittelwert: " + m);
 
 
+    }
+
+    public void turnOn(){
+
+        battery.get(0).setChargingStatus(battery.get(0).chargingStatus -5);
+        battery.get(1).setChargingStatus(battery.get(1).chargingStatus -5);
+        System.out.println("neuer Wert:" + battery.get(0).chargingStatus);
+        System.out.println("neuer Wert:" + battery.get(1).chargingStatus);
+
+    }
+
+    public void turnOff(){
+        System.out.println("The remote is now offline");
+        hasPower = false;
     }
 
 }
--- Aufgaben/src/main.java
+++ Aufgaben/src/main.java
@@ -2,7 +2,16 @@ public class main {
     public static void main(String[] args) {
         Battery b1 = new Battery(28);
         Battery b2 = new Battery(23);
-        Remote r1 = new Remote(true,24);
+        Remote r1 = new Remote(true,false);
+        r1.addBattery(b1);
+        r1.addBattery(b2);
         r1.getStatus();
+
+        if(r1.isHasPower() == false){
+            r1.turnOff();
+        }else{
+            r1.turnOn();
+        }
+
     }
 }
